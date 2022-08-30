f475972ff4b0da34ac8998ff532503e53b2d42ed
GGbeng <15010976823@163.com>
2022-08-30 10:57:58 +0800

    feat: 更新配置

--- docs/chunks/ThemeProvider.astro_astro_type_script_index_0_lang.cf2a8936.js
+++
@@ -1,1 +0,0 @@
-const e=document.firstElementChild,t=localStorage.getItem("theme");t!==null&&e.setAttribute("data-theme",t);
--- docs/hoisted.a4813757.js
+++
@@ -1,1 +0,0 @@
-import"./chunks/ThemeProvider.astro_astro_type_script_index_0_lang.cf2a8936.js";
