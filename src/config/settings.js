1b99dac8adc3ab0b799f185b0c8082f858d02b3f
GGbeng <15010976823@163.com>
2022-08-30 16:52:06 +0800

    feat: 优化seo

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,7 +1,7 @@
 export default {
-	title: `Odyssey Astro Theme | A Marketing Website Theme for Startups and Businesses`,
-	description: `A simple, clean, and modern theme for a startup or businesses' marketing website.`,
-	url: `https://odyssey-theme.littlesticks.dev`, // No trailing slash!
-	name: `Odyssey`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+	title: `GGbeng | GGbeng blog`,
+	description: `GGbeng site | GGbeng blog`,
+	url: `https://www.ggbeng.tech/`, // No trailing slash!
+	name: `GGbeng`, // The short name of the business or brand name. Used for things like the copyright in the footer.
   enableThemeSwitcher: true,
 };
